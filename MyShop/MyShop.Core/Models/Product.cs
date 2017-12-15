d0cd7d53c0f0c1592db4ac35287f06ffeef320ee
Jotejeda <jorgetejeda0804@gmail.com>
2017-12-15 15:33:24 -0400

    Creacion de las propiedades y metodos de ejecucion

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,100)]
+        public decimal Range { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
